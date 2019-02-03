2261c0f5baa31c35438acdaf17d7347cd6fd56ac
mathec-x <40206389+mathec-x@users.noreply.github.com>
2019-02-03 14:00:37 -0200

    Add direct/disconnect/connect

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ var http=require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 5000;
 var socketsOn = [];
-
-
+var connections = [];
 
 
 //ONLY FOR SECURITY
@@ -15,52 +14,48 @@ var helmet = require('helmet');
 io.on('connection', function(socket) {
 	//include socket on JobId
 	var newjoin = socket.handshake.query.Token;
+	// join into new group
 	socket.join(newjoin);
-
+	//catch user login on the querystring
 	var user = socket.handshake.query.Login;
-	socketsOn.push({user: user, socket: socket});
+	//verify if user is present
+	if (socketsOn[user]) {
+		console.log('Duplicate user '+ user);
+		socketsOn[user].emit('Package', {io: 'Duplicate', user: user});
+		socketsOn.splice(socketsOn[user], 0);
+	}
+	//store user by login and self sokcet id
+		socketsOn[user] = socket;
+		    // console.clear(); console.log('Rooms =>'+Object.keys(io.sockets.adapter.rooms));
+			connections = Object.keys(socketsOn)
 
-
-	  console.clear();
-	  console.log('Rooms =>'+Object.keys(io.sockets.adapter.rooms));
-	  for (var i = socketsOn.length - 1; i >= 0; i--) {
-		  console.log(socketsOn[i].user);
-	  }
-
+			    // emit user when new connection
+				io.to(newjoin).emit('Package', {io: 'New', user: user, online: connections});
+				console.clear();console.log(connections);
+				//console.log(user +' Conectou');
+	
+    // emit disconect client
     socket.on('disconnect', function() {
-
-      socketsOn.splice(socket, 1);
-
-	  console.clear();
-	  console.log('Rooms =>'+Object.keys(io.sockets.adapter.rooms));
-	  for (var i = socketsOn.length - 1; i >= 0; i--) {
-		  console.log(socketsOn[i].user);
-	  }
-	      
+	    delete socketsOn[user];
+			connections = Object.keys(socketsOn);
+			    // emit users when new disconnetcion
+				//console.clear();console.log(connections);
+				console.log(user +' desconectou');
+		   				 io.to(newjoin).emit('Package', {io: 'Disconnect', user: user});	      
 	});
 
+    // chamada a cada interação da tela
 	socket.on('Package', ($data)=>{
 		if ($data.direct) {
-	
-			  for (var i = socketsOn.length - 1; i >= 0; i--) {
-				  if (socketsOn[i].user == $data.direct) {				  	
-					console.log(user +' Enviou para: ' + $data.direct);
-					var cli = socketsOn[i].socket;
-
-						 cli.emit('Package', $data);
-			
-					break;
-				  } else {
-
-				  	 console.log(user +' Enviou para: ' + $data.direct + ' usuário estava deslogado');
-
-				  }
-			  }
+			// se existir um atributo direct
+			var cli = socketsOn[$data.direct];
+			    cli.emit('Package', $data);
+				    console.log('direct to ' + cli);
 
 		} else {	
-		
+			// se não envia para todos
 			io.to(newjoin).emit('Package', $data);
-			console.log(user +' Enviou a todos');
+				console.log(user +' Enviou a todos');
 		}
 
 	});
