13ac5acb4b5e5a6f0ab9b1834f089f54126cd760
mathec-x <40206389+mathec-x@users.noreply.github.com>
2019-11-05 21:19:44 -0300

    example add cors

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get('/sendmail', function(req, res) {
     });  
     return false;
 });
-
+io.origins(['http://cicloinsight.com.br/','https://cicloinsight.com.br/', 'http://localhost:70']);
 io.on('connection', function(socket) {
 	if (socket.handshake.query.Syb) {
 		var subGroup = socket.handshake.query.Syb;		
